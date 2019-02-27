c044c4389a96e5f6477383750f74a9761e30c52c
User <User@DEV-022784.OnBase.net>
2019-02-26 19:23:42 -0500

    week 4 code along

---
+++ Week04/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool snowing = true;
+            bool haveCarrot = true;
+            int temp = 40;
+            string clothes = "outfit";
+            string name = "Lucas";
+
+            //if ((snowing && temp <= 40) && (name != "Bob" || haveCarrot)) {
+               // Console.WriteLine("I'm a snowman");
+            //}
+
+            /*if ("Random String" != "Random String") {
+                Console.WriteLine("stuff");
+            }
+            if (15>7) {
+                Console.WriteLine("yay");
+            }
+            if (27<=152) {
+                Console.WriteLine("whoop whoop");
+            }
+            if ("My name is jordan" == "my name is jordan") {
+                Console.WriteLine("fun");
+            }
+            if (35>=35) {
+                Console.WriteLine("great");
+            }
+            if (3.1254798 <= 5) {
+                Console.WriteLine("cool");
+            }
+            if (9001<9001) {
+                Console.WriteLine("i dont care");
+            }
+            if (false || false) {
+                Console.WriteLine("okay");
+            }
+            if (true || false) {
+                Console.WriteLine("sounds good");
+            }
+            if (true && false) {
+                Console.WriteLine("spectacular");
+            } */
+            Console.WriteLine("What day of the week is it?");
+            string day = Console.ReadLine();
+            Console.WriteLine("What time of day is it?");
+            string time = Console.ReadLine();
+            Console.WriteLine("How long will you be parking for?");
+            string park = Console.ReadLine();
+            if (day == "Monday");
+        }
+    }
+}
