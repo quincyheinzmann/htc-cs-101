fc76f46a7e615514581f280a7ada3475a119bbd5
User <User@DEV-022784.OnBase.net>
2019-03-12 19:26:10 -0400

    Even or odd

---
+++ Week05/week 06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace week_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int userInput =Convert.ToInt32(Console.ReadLine());
+            printEven(userInput);
+        }
+         public static void printEven(int evenOrOdd)
+         {
+            int x = 0;
+             while (x >= 100){
+
+             if (evenOrOdd % 2 == 0){
+                 Console.WriteLine("Your number is even");
+             }
+             else 
+                {
+                 Console.WriteLine("Your number is odd");
+                }
+
+            evenOrOdd++;
+             }
+         }
+    }
+}
